8e6fc9f3bad1700afe4a4835d3c5cbe4913f31a5
JeminDangashiya <jemindangashiya90728@gmail.com>
2021-07-18 09:17:31 +0530

    form creation

--- index.js
+++ index.js
@@ -10,5 +10,21 @@ app.set('view engine', 'html');
 // for using the static files in node js, register it with express
 app.use(express.static(__dirname + '/public'));
 
+app.get("/", function (req, res) {
+    res.send('<a href="/Login">login</a><a href="/register">Register</a>');
+})
+app.get('/login', function (req, res) {
+    res.render('login');
+})
 
-app.listen(8080)
+
+
+app.get('/login', function (req, res) {
+    res.render('login');
+})
+app.get('/register', function (req, res) {
+    res.render('register');
+})
+
+
+app.listen(8080);
